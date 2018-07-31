d8cac3f6e45141018f6eb25f2a1820fc2f51928c
nimetbetul <nbkaratas@students.chccs.k12.nc.us>
2018-07-31 15:40:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+  get '/' do
+    "Welcome to my life!"
+  end
+  
+  get '/name' do
+    "My name is NIMET!"
+  end
+  
+  get '/hometown' do
+    "My hometown is Istanbul"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is 'Better Now' by Post Malone"
+  end 
+end 
+
